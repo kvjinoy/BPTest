1f707e2a5d24288616c0360d98070ff20868371d
kvjinoy <kvjinoy@gmail.com>
2015-01-16 12:36:09 -0500

    first checkin
    https://github.com/kvjinoy/BPTest

---
+++ Constants/ContentType.cs
@@ -0,0 +1,8 @@
+﻿namespace BoilerTest.Constants
+{
+    public static class ContentType
+    {
+        public const string Text = "text/plain";
+        public const string Xml = "application/xml";
+    }
+}
